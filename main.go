5e61546311e0690a508a96a5276e945717a99fe6
CP Payne <charlethical1@gmail.com>
2024-03-24 20:33:01 +0200

    Created server and cors middleware

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	corsMux := middlewareCors(mux)
+
+	srv := http.Server{
+		Addr:    ":8000",
+		Handler: corsMux,
+	}
+	fmt.Printf("Listening on port %s\n", srv.Addr)
+	err := srv.ListenAndServe()
+	// err := http.ListenAndServe(":8000", corsMux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
