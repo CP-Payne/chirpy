7b85ef12b9f392da7c166dc603c9d1591af46bbe
CP Payne <charlethical1@gmail.com>
2024-03-25 21:17:32 +0200

    Added handler to track visits to home page

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/CP-Payne/chirpy
 
-go 1.21.1
+go 1.22.1
--- main.go
+++ main.go
@@ -6,17 +6,32 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
 func main() {
 	mux := http.NewServeMux()
 
+	cfg := &apiConfig{
+		fileserverHits: 0,
+	}
+
 	corsMux := middlewareCors(mux)
 
-	srv := http.Server{
+	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: corsMux,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	fileServer := http.FileServer(http.Dir("."))
+
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", http.HandlerFunc(cfg.handlerMetrics))
+	mux.HandleFunc("/api/reset", http.HandlerFunc(cfg.handlerReset))
+
+	// mux.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets/"))))
 
 	fmt.Printf("Listening on port %s\n", srv.Addr)
 	err := srv.ListenAndServe()
@@ -37,4 +52,47 @@ func middlewareCors(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	// if r.Method != http.MethodGet {
+	// 	http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	// 	return
+	// }
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	// if r.Method != http.MethodGet {
+	// 	http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	// 	return
+	// }
+	// TODO
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	// hits := fmt.Sprintf("Hits: %d", cfg.fileserverHits)
+	fmt.Fprintf(w, `<html>
+	  <body>
+	    <h1>Welcome, Chirpy Admin</h1>
+	    <p>Chirpy has been visited %d times!</p>
+	  </body>
+	</html>`, cfg.fileserverHits)
+
+	// w.Write([]byte(hits))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	// TODO
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
