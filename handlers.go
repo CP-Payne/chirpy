acfc92090179abc29f714e882a35a805e028332b
CP Payne <charlethical1@gmail.com>
2024-03-28 07:26:47 +0200

    Refactored code

---
+++ handlers.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	// if r.Method != http.MethodGet {
+	// 	http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	// 	return
+	// }
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	// if r.Method != http.MethodGet {
+	// 	http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	// 	return
+	// }
+	// TODO
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	// hits := fmt.Sprintf("Hits: %d", cfg.fileserverHits)
+	fmt.Fprintf(w, `<html>
+	  <body>
+	    <h1>Welcome, Chirpy Admin</h1>
+	    <p>Chirpy has been visited %d times!</p>
+	  </body>
+	</html>`, cfg.fileserverHits)
+
+	// w.Write([]byte(hits))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	// TODO
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
+	respChirps, err := cfg.DB.GetChirps()
+	if err != nil {
+		fmt.Println(err)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps")
+	}
+
+	respondWithJSON(w, http.StatusOK, respChirps)
+
+}
+
+func (cfg *apiConfig) handlerGetChirp(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("chirp_id")
+	intId, err := strconv.Atoi(id)
+	if err != nil {
+		fmt.Println(err)
+		respondWithError(w, http.StatusBadRequest, "Invalid chirp id")
+		return
+	}
+
+	chirp, err := cfg.DB.GetChirp(intId)
+	if err != nil {
+		fmt.Println(err)
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
+		return
+	}
+	if chirp.ID == 0 {
+		respondWithError(w, http.StatusNotFound, http.StatusText(http.StatusNotFound))
+		return
+
+	}
+
+	respondWithJSON(w, http.StatusOK, chirp)
+
+}
+
+func (cfg *apiConfig) handlerAddChirp(w http.ResponseWriter, r *http.Request) {
+	type params struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	chirpText := params{}
+	err := decoder.Decode(&chirpText)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode paramters")
+		return
+	}
+
+	if len(chirpText.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	badWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+
+	cleandedChirp := cleanChirp(chirpText.Body, badWords)
+
+	chirp, err := cfg.DB.CreateChirp(cleandedChirp)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	respondWithJSON(w, http.StatusCreated, chirp)
+}
+
+func cleanChirp(chirpText string, badWords map[string]struct{}) (cleanedChirp string) {
+	textSlice := strings.Split(chirpText, " ")
+	for i, word := range textSlice {
+		loweredWord := strings.ToLower(word)
+		if _, ok := badWords[loweredWord]; ok {
+			textSlice[i] = "****"
+		}
+	}
+	return strings.Join(textSlice, " ")
+}
