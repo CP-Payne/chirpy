144161f264e2f00fdad1ce4cc3bab3c1e82f3fe5
CP Payne <charlethical1@gmail.com>
2024-03-28 17:27:31 +0200

    Refactored userHandler

--- userHandler.go
+++ userHandler.go
@@ -5,18 +5,23 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/CP-Payne/chirpy/internal/auth"
 	"github.com/CP-Payne/chirpy/internal/database"
-	"golang.org/x/crypto/bcrypt"
 )
+
+type User struct {
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"-"`
+}
 
 func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
 	type params struct {
 		Email    string `json:"email"`
 		Password string
 	}
-	type respBody struct {
-		ID    int    `json:"id"`
-		Email string `json:"email"`
+	type response struct {
+		User
 	}
 	decoder := json.NewDecoder(r.Body)
 	userParams := params{}
@@ -36,7 +41,7 @@ func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userParams.Password), 0)
+	hashedPassword, err := auth.HashPassword(userParams.Password)
 	if err != nil {
 		fmt.Println(err)
 		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
@@ -49,7 +54,7 @@ func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) 
 		fmt.Println(err)
 		return
 	}
-	respondWithJSON(w, http.StatusCreated, respBody{ID: user.ID, Email: user.Email})
+	respondWithJSON(w, http.StatusCreated, response{User{ID: user.ID, Email: user.Email}})
 }
 
 func (cfg *apiConfig) handlerLoginUser(w http.ResponseWriter, r *http.Request) {
@@ -57,9 +62,8 @@ func (cfg *apiConfig) handlerLoginUser(w http.ResponseWriter, r *http.Request) {
 		Email    string `json:"email"`
 		Password string
 	}
-	type respBody struct {
-		ID    int    `json:"id"`
-		Email string `json:"email"`
+	type response struct {
+		User
 	}
 	decoder := json.NewDecoder(r.Body)
 	userParams := params{}
@@ -84,12 +88,12 @@ func (cfg *apiConfig) handlerLoginUser(w http.ResponseWriter, r *http.Request) {
 		}
 
 	}
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userParams.Password))
+	err = auth.CheckPasswordHash(userParams.Password, user.Password)
 	if err != nil {
 		respondWithError(w, http.StatusUnauthorized, "Incorrect credentials")
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, respBody{ID: user.ID, Email: user.Email})
+	respondWithJSON(w, http.StatusOK, response{User{ID: user.ID, Email: user.Email}})
 
 }
